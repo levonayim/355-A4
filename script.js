e978dcc90cbff38a79cf2d5a6213134c818e654d
Levona Yim <levona10@hotmail.com>
2018-03-07 22:09:41 -0800

    added a legend on bottom
    legend is still buggy.. need to see how to group it

--- script.js
+++ script.js
@@ -5,11 +5,6 @@ var margin = {top: 30, right: 50, bottom: 50, left: 70},
 
 // SET RANGES OF SCALE
 var x = d3.scale.linear().range([0, width]);
-
-//ACTUAL X-AXIS LABELS WE WANTED TO USE
-// var x = d3.scale.ordinal()
-//     .domain(["Under $10k", "$10k to $19,999", "$20k to $29,999", "$30k to $39,999", "$40k to $49,999", "$50k to $59,999", "$60k to $69,999", "$70k to $79,999", "$80k to $89,999", "$90k to $99,999", "$100k and over"])
-//     .rangePoints([0, width]);
 var y = d3.scale.linear().range([height, 0]);
 
 //COLOUR RANGE
@@ -34,12 +29,151 @@ var svg = d3.select("body")
 // group that will contain all of the plots
     var groups = svg.append("g").attr("transform", "translate(0)");
 
-//tool tip for when user hovers over dots, display information
-    var tooltip = d3.select('body')
-      .append('div')
-      .attr('class', 'tooltip')
-      .style("opacity", 0);
-
+// array of the cities, used for the legend
+    var allcities = ["Abbotsford - Mission", "Alma", "Arnprior", "Baie-Comeau", "Barrie",
+"Bathurst","Bay Roberts","Belleville","Brandon","Brantford","Brockville","Brooks","Calgary",
+"Campbell River","Campbellton","Campbellton (New Brunswick part)",
+"Campbellton (Quebec part)","Camrose","Canmore",
+"Cape Breton",
+"Carleton Place",
+"Centre Wellington",
+"Charlottetown",
+"Chatham-Kent",
+"Chilliwack",
+"Cobourg",
+"Cold Lake",
+"Collingwood",
+"Corner Brook",
+"Cornwall",
+"Courtenay",
+"Cowansville",
+"Cranbrook",
+"Dawson Creek",
+"Dolbeau-Mistassini",
+"Drummondville",
+"Duncan",
+"Edmonton",
+"Edmundston",
+"Elliot Lake",
+"Estevan",
+"Fort St. John",
+"Fredericton",
+"Gander",
+"Granby",
+"Grand Falls-Windsor",
+"Grande Prairie",
+"Greater Sudbury",
+"Guelph",
+"Halifax",
+"Hamilton",
+"Hawkesbury",
+"Hawkesbury (Ontario part)",
+"Hawkesbury (Quebec part)",
+"High River",
+"Ingersoll",
+"Joliette",
+"Kamloops",
+"Kawartha Lakes",
+"Kelowna",
+"Kenora",
+"Kentville",
+"Kingston",
+"Kitchener - Cambridge - Waterloo",
+"Lachute",
+"Lacombe",
+"Leamington",
+"Lethbridge",
+"Lloydminster",
+"Lloydminster (Alberta part)",
+"Lloydminster (Saskatchewan part)",
+"London",
+"Matane",
+"Medicine Hat",
+"Midland",
+"Miramichi",
+"Moncton",
+"Montreal",
+"Moose Jaw",
+"Nanaimo",
+"Nelson",
+"New Glasgow",
+"Norfolk",
+"North Battleford",
+"North Bay",
+"Okotoks",
+"Orillia",
+"Oshawa",
+"Ottawa - Gatineau",
+"Ottawa - Gatineau (Ontario part)",
+"Ottawa - Gatineau (Quebec part)",
+"Owen Sound",
+"Parksville",
+"Pembroke",
+"Penticton",
+"Petawawa",
+"Peterborough",
+"Port Alberni",
+"Port Hope",
+"Portage la Prairie",
+"Powell River",
+"Prince Albert",
+"Prince George",
+"Prince Rupert",
+"Quesnel",
+"Red Deer",
+"Regina",
+"Rimouski",
+"Rivire-du-Loup",
+"Rouyn-Noranda",
+"Saguenay",
+"Saint John",
+"Saint-Georges",
+"Saint-Hyacinthe",
+"Sainte-Marie",
+"Salaberry-de-Valleyfield",
+"Salmon Arm",
+"Sarnia",
+"Saskatoon",
+"Sault Ste. Marie",
+"Sept-‘les",
+"Shawinigan",
+"Sherbrooke",
+"Sorel-Tracy",
+"Squamish",
+"St. Catharines - Niagara",
+"St. John's",
+"Steinbach",
+"Stratford",
+"Strathmore",
+"Summerside",
+"Swift Current",
+"Sylvan Lake",
+"Terrace",
+"Thetford Mines",
+"Thompson",
+"Thunder Bay",
+"Tillsonburg",
+"Timmins",
+"Toronto",
+"Trois-Rivires",
+"Truro",
+"Val-d'Or",
+"Vancouver",
+"Vernon",
+"Victoria",
+"Victoriaville",
+"Wasaga Beach",
+"Wetaskiwin",
+"Weyburn",
+"Whitehorse",
+"Williams Lake",
+"Windsor",
+"Winkler",
+"Winnipeg",
+"Wood Buffalo",
+"Woodstock",
+"Yellowknife",
+"Yorkton"]
 
 // IMPORTING DATA (CSV FILE)
 d3.csv("data.csv", function(error, data) {
@@ -80,25 +214,14 @@ d3.csv("data.csv", function(error, data) {
         .duration(800).style("opacity", 1)
         .attr("r", 16).ease("elastic");
 
-    /////DISPLAY LABEL OF COORDINATES. 
-    //commented out because it wouldnt work for just a single bubble.
-        // groups.append("g")
-        //    .selectAll("text")
-        //    .data(data)
-        //    .enter()
-        //    .append("text")
-        //    //coordinates for labels of cities on top of dots
-        //    .attr("x",function (d){
-        //        return x(d['income'])-labelSpacing*2 ;
-        //    })
-        //    .attr("y",function (d){
-        //        return y(d['fraction'])-labelSpacing; 
+    // function to move mouseover item to front of SVG stage, when another bubble overlaps it
+    //this snippet was borrowed from http://bl.ocks.org/nsonnad/4481531
+        d3.selection.prototype.moveToFront = function() { 
+          return this.each(function() { 
+            this.parentNode.appendChild(this); 
+          }); 
+        };
 
-        //    })
-        //    .attr("font-family","sans-serif")
-        //    .attr("font-size","9px")
-        //    .attr("fill", "#333333")
-        //    .text(function (d) { return format(d.income)+","+format(d.fraction);})
         
     };
     // mouse out functionality
@@ -116,6 +239,30 @@ d3.csv("data.csv", function(error, data) {
     circles.on("mouseover", mouseOn);
     circles.on("mouseout", mouseOff);
 
+////LEGEND
+// rectangle box holding the colours. having issues at the moment to group everything 
+// so that a new line can be made everytime it reaches max width.
+    var legend = svg.append("g").selectAll("rect")
+            .data(allcities)
+        .enter().append("rect")
+        .attr({
+          x: function(d, i) { return (i*45); },//spacing for each box
+          y: height+35, //how far up/down it should be from the x-axis
+          width: 15, //size of box
+          height: 5
+        })
+        .style("fill", function(d) { return color(d); });
+
+    // legend labels for each city  
+        svg.selectAll("text")
+            .data(allcities)
+        .enter().append("text")
+        .attr({
+        x: function(d, i) { return (i*45); }, //spacing for each city
+        y: height + 50, //how far up/down it should be from the x-axis 
+        })
+        .attr("font-size","7px")
+        .text(function(d) { return d; });
 
 
 //CREATE AXIS
@@ -128,7 +275,7 @@ d3.csv("data.csv", function(error, data) {
         .call((xAxis)   
             .ticks(10) // set details on their ticks on x-axis
             .tickSubdivide(true)
-            .tickSize(10, 10, 0)
+            .tickSize(10, 10, 10)
             .orient("bottom")) 
 
         .append("text")
